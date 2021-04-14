de9b8e3292d4928e7a470c12a1f214250edfa55d
Manuel-condori <2828@holbertonschool.com>
2021-04-14 18:29:05 +0000

    split line

---
+++ lsh_split_line.c
@@ -0,0 +1,39 @@
+#include "shell.h"
+/**
+   *lsh_split_line - split a line into tokens (very naively).
+   *@line: the line
+   *Return: Null-terminated array of tokens
+ */
+char **lsh_split_line(char *line)
+{
+	int bufsize = LSH_TOK_BUFSIZE, position = 0;
+	char **tokens = malloc(bufsize * sizeof(char *));
+	char *token, **tokens_backup;
+
+	if (!tokens)
+	{
+		fprintf(stderr, "./shell: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+	token = strtok(line, LSH_TOK_DELIM);
+	while (token != NULL)
+	{
+		tokens[position] = token;
+		position++;
+		if (position >= bufsize)
+		{
+			bufsize += LSH_TOK_BUFSIZE;
+			tokens_backup = tokens;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+			if (!tokens)
+			{
+				free(tokens_backup);
+				fprintf(stderr, "./shell: allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+		token = strtok(NULL, LSH_TOK_DELIM);
+	}
+	tokens[position] = NULL;
+	return (tokens);
+}
